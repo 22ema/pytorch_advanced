53810bb6685dbf7fdc4767ec49b7ab753871cd34
yunsung han <yunsung9503@gmail.com>
2022-05-10 23:33:55 +0900

    update vgg_model_read.py code

--- study_directory/1_chapter/vgg_model_read.py
+++ study_directory/1_chapter/vgg_model_read.py
@@ -14,23 +14,27 @@ class ILSVRCPredictor():
 
     def predict_max(self, out):
         maxid = np.argmax(out.detach().numpy())
+        print(maxid)
+        print(self.class_index)
         predicted_label_name = self.class_index[str(maxid)][1]
         return predicted_label_name
 
 if __name__ == "__main__":
-    image_path = "./dataset/1.jpg"
+    image_path = "./dataset/goldenretriever-3724972_640.jpg"
     img = Image.open(image_path)
-    plt.imshow(img)
-    plt.show()
+    ILSVRC_class_index = json.load(open("./dataset/imagenet_class_index.json",'r'))
+    predictor = ILSVRCPredictor(ILSVRC_class_index)
     resize = 224
     mean = (0.485, 0.456, 0.406)
     std = (0.299, 0.224, 0.225)
     base_transform = data_transform.BaseTransform(resize, mean, std)
     image_transform = base_transform(img)
-    image_transform = image_transform.numpy().transpose((1,2,0))
-    image_transform = np.clip(image_transform, 0, 1)
-    plt.imshow(image_transform)
-    plt.show()
+    # image_transform = image_transform.numpy().transpose((1,2,0))
+    # image_transform = np.clip(image_transform, 0, 1)
+    inputs = image_transform.unsqueeze_(0)
+    print(inputs.shape)
     use_pretrained = True
     net = models.vgg16(pretrained=use_pretrained)
-    net.eval()
+    out = net(inputs)
+    result = predictor.predict_max(out)
+    print(result)
